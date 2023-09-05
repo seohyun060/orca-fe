b5776b4d161c23aea8aa7b6e758cc15cdd657970
Noah <seohyun060@naver.com>
2023-09-05 16:34:38 +0900

    임시

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './styles/home.styles.css';
+import images from 'src/assets/images';
 import home_banner from '../../assets/images/HOME/home_banner.png';
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div className='home'></div>;
+  return (
+    <div className='home'>
+      <div className='home-anime'>
+        <img src={images.logo_orca} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/components/HomeIntro.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const HomeIntro = (props: Props) => {
+  return <div>HomeIntro</div>;
+};
+
+export default HomeIntro;

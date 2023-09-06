ccc5aab4e85bc450eaed735eb41059040d8c4a89
Noah <seohyun060@naver.com>
2023-09-06 10:57:57 +0900

    research 임시

---
+++ src/pages/Home/components/HomeResearcher.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const HomeResearcher = (props: Props) => {
+  return <div>HomeResearcher</div>;
+};
+
+export default HomeResearcher;
---
+++ src/pages/Home/containers/HomeResearcherContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import HomeResearcher from '../components/HomeResearcher';
+type Props = {};
+
+const HomeResearcherContainer = (props: Props) => {
+  return <HomeResearcher />;
+};
+
+export default HomeResearcherContainer;

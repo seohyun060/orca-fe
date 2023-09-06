b0c3f91200c4f621f75bc3c3060b31a8151944f5
SangHyeon <sanghyeon@beamworks.co.kr>
2023-09-06 11:46:35 +0900

    Home Page 통합

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
-import './styles/home.styles.css';
-import HomeIntroContainer from './containers/HomeIntroContainer';
+import React from "react";
+import "./styles/home.styles.css";
+import "./styles/home.styles.scss";
+import HomeIntroContainer from "./containers/HomeIntroContainer";
+import EventCard from "../Events/components/EventCard";
+import ProjectCard from "../Projects/components/ProjectCard";
 
-import images from 'src/assets/images';
-import home_banner from '../../assets/images/HOME/home_banner.png';
+import images from "src/assets/images";
+import home_banner from "../../assets/images/HOME/home_banner.png";
+
+
 type Props = {};
 const Home = (props: Props) => {
   return (
-    <div className='home'>
-      <div className='home-anime'>
+    <div className="home">
+      <div className="home-anime">
         <img src={images.logo_orca} />
       </div>
       <HomeIntroContainer />
+      <section className='Section'>
+        <div className='SectionTitle'>
+          <label className='ProjectFont'>Projects</label>
+          {/* project로 연결 */}
+          <button className='ViewButton'>
+            <label className='View'>View All</label>
+          </button>
+        </div>
+        <div className='ProjectBox'>
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+      </section>
+      <section className="Section">
+      <div className='SectionTitle'>
+          <label className='ProjectFont'>Events</label>
+          {/* project로 연결 */}
+          <button className='ViewButton'>
+            <label className='View'>View All</label>
+          </button>
+        </div>
+        <div className="SectionTitle">
+          <div className="EventBox">
+            <EventCard />
+            <EventCard />
+            <EventCard />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };

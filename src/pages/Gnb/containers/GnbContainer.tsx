c75f4ae0086772eb720ddb91403dfb680e3501d8
Noah <seohyun060@naver.com>
2023-09-15 10:58:31 +0900

    임시

--- src/pages/Gnb/containers/GnbContainer.tsx
+++ src/pages/Gnb/containers/GnbContainer.tsx
@@ -3,107 +3,116 @@ import Gnb from '../Gnb';
 import { GNBTableTypes } from '@typedef/types';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useTranslation } from "react-i18next"
+import { useTranslation } from 'react-i18next';
 type Props = { location: string };
 const GnbContainer = ({ location }: Props) => {
-  const route = location.split('/')[1];
-  const navigate = useNavigate();
-  const tabTable: GNBTableTypes[] = [
-    {
-      label: 'ORCA',
-      path: '/orca',
-    },
-    {
-      label: 'Researcher',
-      path: '/researcher',
-    },
-    {
-      label: 'Projects',
-      path: '/projects',
-    },
-    {
-      label: 'Events',
-      path: '/events',
-    },
-    {
-      label: 'Insights',
-      path: '/insights',
-    },
-  ];
-  const [selectedTab, setSelectedTab] = useState<string>('/');
-  const [language, setLanguage] = useState('En');
-  const [gnbColor, setGnbColor] = useState('');
-  const [scrollPosition, setScrollPosition] = useState(0);
-  
-  const { i18n, t } = useTranslation();
+	const route = location.split('/')[1];
+	const navigate = useNavigate();
+	const tabTable: GNBTableTypes[] = [
+		{
+			label: 'ORCA',
+			path: '/orca',
+		},
+		{
+			label: 'Researcher',
+			path: '/researcher',
+		},
+		{
+			label: 'Projects',
+			path: '/projects',
+		},
+		{
+			label: 'Events',
+			path: '/events',
+		},
+		{
+			label: 'Insights',
+			path: '/insights',
+		},
+	];
+	const [selectedTab, setSelectedTab] = useState<string>('/');
+	const [language, setLanguage] = useState('En');
+	const [gnbColor, setGnbColor] = useState('');
+	const [scrollPosition, setScrollPosition] = useState(0);
+	const [mobile, setMobile] = useState(false);
+	const { i18n, t } = useTranslation();
 
-  const onItemClicked = useCallback((path: string) => {
-    if (path === '/custom') {
-      return;
-    }
-    setSelectedTab(path);
-    navigate(path);
-  }, []);
-  const updateScroll = () => {
-    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
-  };
+	const onItemClicked = useCallback((path: string) => {
+		if (path === '/custom') {
+			return;
+		}
+		setSelectedTab(path);
+		navigate(path);
+	}, []);
+	const updateScroll = () => {
+		setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+	};
 
-  const gnbColorHandler = useCallback(() => {
-    if (`/${route}` == '/') {
-      if (scrollPosition > 2000) {
-        setGnbColor('-white');
-      } else {
-        setGnbColor('');
-      }
-    } else if (`/${route}` == '/orca') {
-      if (scrollPosition > 727) {
-        setGnbColor('-white');
-      } else {
-        setGnbColor('');
-      }
-    } else if (`/${route}` == '/researcher') {
-      if (scrollPosition > 1081) {
-        setGnbColor('-white');
-      } else {
-        setGnbColor('');
-      }
-    } else if (`/${route}` == '/projects') {
-      setGnbColor('-white');
-    } else if (`/${route}` == '/researcherdetail') {
-      setGnbColor('-white');
-    } else if (`/${route}` == '/insights') {
-      setGnbColor('-white');
-    } else if (`/${route}` == '/events') {
-      setGnbColor('-white');
-    }
-  }, [scrollPosition, route]);
+	const gnbColorHandler = useCallback(() => {
+		if (`/${route}` == '/') {
+			if (scrollPosition > 2000) {
+				setGnbColor('-white');
+			} else {
+				setGnbColor('');
+			}
+		} else if (`/${route}` == '/orca') {
+			if (scrollPosition > 727) {
+				setGnbColor('-white');
+			} else {
+				setGnbColor('');
+			}
+		} else if (`/${route}` == '/researcher') {
+			if (scrollPosition > 1081) {
+				setGnbColor('-white');
+			} else {
+				setGnbColor('');
+			}
+		} else if (`/${route}` == '/projects') {
+			setGnbColor('-white');
+		} else if (`/${route}` == '/researcherdetail') {
+			setGnbColor('-white');
+		} else if (`/${route}` == '/insights') {
+			setGnbColor('-white');
+		} else if (`/${route}` == '/events') {
+			setGnbColor('-white');
+		}
+	}, [scrollPosition, route]);
 
-  const onLanguageClicked = useCallback(() => {
-    if (language === 'Kr') {
-      setLanguage('En');
-      i18n.changeLanguage('en')
-    } else {
-      setLanguage('Kr');
-      i18n.changeLanguage('kr')
-    }
-  }, [language]);
+	const onLanguageClicked = useCallback(() => {
+		if (language === 'Kr') {
+			setLanguage('En');
+			i18n.changeLanguage('en');
+		} else {
+			setLanguage('Kr');
+			i18n.changeLanguage('kr');
+		}
+	}, [language]);
+	// ... (이전 코드)
 
-  useEffect(() => {
-    window.addEventListener('scroll', updateScroll);
-    gnbColorHandler();
-  }, [scrollPosition, route]);
-  return (
-    <div>
-      <Gnb
-        gnbColor={gnbColor}
-        tabTable={tabTable}
-        language={language}
-        route={route}
-        onLanguageClicked={onLanguageClicked}
-        onItemClicked={onItemClicked}
-      />
-    </div>
-  );
+	// ... (나머지 코드)
+
+	useEffect(() => {
+		window.addEventListener('scroll', updateScroll);
+		gnbColorHandler();
+		// if (window.innerWidth < 768) {
+		// 	setMobile(true);
+		// } else {
+		// 	setMobile(false);
+		// }
+		console.log(mobile);
+	}, [scrollPosition, route, gnbColorHandler]);
+	return (
+		<div>
+			<Gnb
+				gnbColor={gnbColor}
+				tabTable={tabTable}
+				language={language}
+				route={route}
+				onLanguageClicked={onLanguageClicked}
+				onItemClicked={onItemClicked}
+			/>
+		</div>
+	);
 };
 
 export default GnbContainer;

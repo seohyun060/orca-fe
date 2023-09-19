fee4d86e597056f037454fecd80f0304bd7169ff
Noah <seohyun060@naver.com>
2023-09-19 15:48:23 +0900

    insight transition 추가
    insight, researcher fold 방식 변경 후 transition 적용

--- src/pages/Insights/Insights.tsx
+++ src/pages/Insights/Insights.tsx
@@ -1,117 +1,140 @@
 import React from 'react';
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 import './style/Insight.css';
 import InsightsCard from './components/InsightsCard';
 import { Insight } from '@typedef/types';
+import { motion, AnimatePresence } from 'framer-motion';
+
 type Props = {
-  insightsGnbHandler: (e: string) => void;
-  filteredList: Insight[];
-  readMore: boolean;
-  onReadMoreClick: () => void;
-  containerHeight: string;
-  selectedTab: number;
+	insightsGnbHandler: (e: string) => void;
+	filteredList: Insight[];
+	readMore: boolean;
+	onReadMoreClick: () => void;
+	containerHeight: string;
+	selectedTab: number;
+	prevHeight: string;
+	slicedList: Insight[];
 };
 
 const Insights = ({
-  insightsGnbHandler,
-  filteredList,
-  readMore,
-  onReadMoreClick,
-  containerHeight,
-  selectedTab,
+	insightsGnbHandler,
+	filteredList,
+	readMore,
+	onReadMoreClick,
+	containerHeight,
+	selectedTab,
+	prevHeight,
+	slicedList,
 }: Props) => {
-  const { t } = useTranslation();
-  return (
-    <div className='insights'>
-      <div className='insights-head'>
-        <div className='big'>ORCA Insights:</div>
-        <div className='small'>{t("news")}</div>
-      </div>
-      <div className='insights-gnb'>
-        <div className='insights-gnb-body'>
-          {/* 추후에 동적생성? */}
-          <button
-            onClick={() => {
-              insightsGnbHandler('all');
-              console.log(filteredList);
-            }}
-            style={{
-              backgroundColor: selectedTab == 0 ? '#000' : '#fff',
-              color: selectedTab == 0 ? '#fff' : '#9E9E9E',
-            }}
-          >
-            ORCA Insights
-          </button>
-          <button
-            onClick={() => {
-              insightsGnbHandler('whitepaper');
-            }}
-            style={{
-              backgroundColor: selectedTab == 1 ? '#000' : '#fff',
-              color: selectedTab == 1 ? '#fff' : '#9E9E9E',
-            }}
-          >
-            White Paper
-          </button>
-          <button
-            onClick={() => {
-              insightsGnbHandler('publication');
-            }}
-            style={{
-              backgroundColor: selectedTab == 2 ? '#000' : '#fff',
-              color: selectedTab == 2 ? '#fff' : '#9E9E9E',
-            }}
-          >
-            Publication
-          </button>
-          <button
-            onClick={() => {
-              insightsGnbHandler('news');
-            }}
-            style={{
-              backgroundColor: selectedTab == 3 ? '#000' : '#fff',
-              color: selectedTab == 3 ? '#fff' : '#9E9E9E',
-            }}
-          >
-            News
-          </button>
-          <button
-            onClick={() => {
-              insightsGnbHandler('education');
-            }}
-            style={{
-              backgroundColor: selectedTab == 4 ? '#000' : '#fff',
-              color: selectedTab == 4 ? '#fff' : '#9E9E9E',
-            }}
-          >
-            Education
-          </button>
-        </div>
-      </div>
-      <div
-        className='insights-body'
-        style={{
-          maxHeight: containerHeight,
-        }}
-      >
-        {filteredList.map((insight, index) => (
-          <div className='insight-info'>
-            <div className='insight-info-box' />
-            <div className='insight-info-type'>{insight.type}</div>
-            <div className='insight-info-title'>{insight.title}</div>
-            <div className='insight-info-date'>{insight.date}</div>
-          </div>
-        ))}
-      </div>
-      {filteredList.length > 12 ? (
-        <button className='insights-readmore' onClick={onReadMoreClick}>
-          {readMore ? 'Fold' : 'Read More'}
-        </button>
-      ) : (
-        ''
-      )}
-    </div>
-  );
+	const { t } = useTranslation();
+	return (
+		<div className='insights'>
+			<div className='insights-head'>
+				<div className='big'>ORCA Insights:</div>
+				<div className='small'>{t('news')}</div>
+			</div>
+			<div className='insights-gnb'>
+				<div className='insights-gnb-body'>
+					{/* 추후에 동적생성? */}
+					<button
+						onClick={() => {
+							insightsGnbHandler('all');
+							console.log(filteredList);
+						}}
+						style={{
+							backgroundColor: selectedTab == 0 ? '#000' : '#fff',
+							color: selectedTab == 0 ? '#fff' : '#9E9E9E',
+						}}
+					>
+						ORCA Insights
+					</button>
+					<button
+						onClick={() => {
+							insightsGnbHandler('whitepaper');
+						}}
+						style={{
+							backgroundColor: selectedTab == 1 ? '#000' : '#fff',
+							color: selectedTab == 1 ? '#fff' : '#9E9E9E',
+						}}
+					>
+						White Paper
+					</button>
+					<button
+						onClick={() => {
+							insightsGnbHandler('publication');
+						}}
+						style={{
+							backgroundColor: selectedTab == 2 ? '#000' : '#fff',
+							color: selectedTab == 2 ? '#fff' : '#9E9E9E',
+						}}
+					>
+						Publication
+					</button>
+					<button
+						onClick={() => {
+							insightsGnbHandler('news');
+						}}
+						style={{
+							backgroundColor: selectedTab == 3 ? '#000' : '#fff',
+							color: selectedTab == 3 ? '#fff' : '#9E9E9E',
+						}}
+					>
+						News
+					</button>
+					<button
+						onClick={() => {
+							insightsGnbHandler('education');
+						}}
+						style={{
+							backgroundColor: selectedTab == 4 ? '#000' : '#fff',
+							color: selectedTab == 4 ? '#fff' : '#9E9E9E',
+						}}
+					>
+						Education
+					</button>
+				</div>
+			</div>
+			<div
+				className='insights-body'
+				style={{
+					maxHeight: 'fit-content',
+				}}
+			>
+				<AnimatePresence>
+					{filteredList.map((insight, index) => {
+						// 첫 12개만 출력하거나 readMore가 true면 모두 출력
+						if (!readMore && index >= 12) return null;
+
+						return (
+							<motion.div
+								className='insight-info'
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}
+								transition={{
+									duration: 0.3,
+									delay: 0.05 * (readMore ? index - 12 : index),
+								}}
+								key={`insight-${index}`}
+							>
+								<div className='insight-info-box' />
+								<div className='insight-info-type'>{insight.type}</div>
+								<div className='insight-info-title'>{insight.title}</div>
+								<div className='insight-info-date'>{insight.date}</div>
+							</motion.div>
+						);
+					})}
+				</AnimatePresence>
+			</div>
+			{filteredList.length > 12 ? (
+				<button className='insights-readmore' onClick={onReadMoreClick}>
+					{readMore ? 'Fold' : 'Read More'}
+				</button>
+			) : (
+				''
+			)}
+		</div>
+	);
 };
 
 export default Insights;
--- src/pages/Insights/containers/InsightsContainer.tsx
+++ src/pages/Insights/containers/InsightsContainer.tsx
@@ -25,90 +25,135 @@ for (let i = 0; i < 20; i++) {
 }
 
 const InsightsContainer = (props: Props) => {
-	const [filteredList, setFilteredList] = useState<Insight[]>(insightList);
+	const [filteredList, setFilteredList] = useState<Insight[]>([]);
+
 	const [readMore, setReadMore] = useState(false);
 	const [containerHeight, setContainerHeight] = useState('1314px');
+	const [prevHeight, setPrevHeight] = useState('1314px');
+	const [slicedList, setSlicedList] = useState<Insight[]>([]);
+
 	const [selectedTab, setSelectedTab] = useState(0);
 	const onReadMoreClick = useCallback(() => {
-		if (filteredList.length > 12) {
+		if (insightList.length > 12) {
+			setPrevHeight(containerHeight);
 			setReadMore((prev) => !prev);
 		}
-	}, [readMore]);
+	}, [readMore, filteredList]);
 	const insightsGnbHandler = useCallback(
 		(type: string) => {
 			switch (type) {
 				case 'all':
-					setFilteredList(insightList);
+					//setFilteredList(insightList);
 					setSelectedTab(0);
 					break;
 				case 'whitepaper':
-					setFilteredList(
-						insightList.filter((insight) => insight.type == 'White Paper'),
-					);
+					// setFilteredList(
+					// 	insightList.filter((insight) => insight.type == 'White Paper'),
+					// );
 					setSelectedTab(1);
 					console.log(selectedTab);
 					break;
 				case 'publication':
-					setFilteredList(
-						insightList.filter((insight) => insight.type == 'Publication'),
-					);
+					// setFilteredList(
+					// 	insightList.filter((insight) => insight.type == 'Publication'),
+					// );
 					setSelectedTab(2);
 					console.log(selectedTab);
 					break;
 				case 'news':
-					setFilteredList(
-						insightList.filter((insight) => insight.type == 'News'),
-					);
+					// setFilteredList(
+					// 	insightList.filter((insight) => insight.type == 'News'),
+					// );
 					setSelectedTab(3);
 					break;
 				case 'education':
-					setFilteredList(
-						insightList.filter((insight) => insight.type == 'Education'),
-					);
+					// setFilteredList(
+					// 	insightList.filter((insight) => insight.type == 'Education'),
+					// );
 					setSelectedTab(4);
 					break;
 			}
+			//setSlicedList(filteredList);
 			console.log(selectedTab);
 			console.log(window.innerWidth);
 		},
-		[filteredList, selectedTab],
+		[filteredList, selectedTab, slicedList],
 	);
-	const handleResize = () => {
-		if (window.innerWidth > 1400) {
-			if (filteredList.length > 12 && !readMore) {
-				setContainerHeight('1314px');
-			} else {
-				setContainerHeight('fit-content');
-			}
-		} else if (window.innerWidth > 1024) {
-			console.log(readMore, window.innerWidth, containerHeight, 'ddd');
-			if (filteredList.length > 12 && !readMore) {
-				setContainerHeight('992px');
-			} else {
-				setContainerHeight('fit-content');
-			}
-		} else if (window.innerWidth > 768) {
-			if (filteredList.length > 12 && !readMore) {
-				setContainerHeight('771.64px');
-			} else {
-				setContainerHeight('fit-content');
-			}
-		} else {
-			if (filteredList.length > 12 && !readMore) {
-				setContainerHeight('870px');
-			} else {
-				setContainerHeight('fit-content');
-			}
-		}
-	};
+	// const handleResize = () => {
+	// 	if (window.innerWidth > 1400) {
+	// 		if (filteredList.length > 12 && !readMore) {
+	// 			setContainerHeight('1314px');
+	// 		} else {
+	// 			setContainerHeight('fit-content');
+	// 		}
+	// 	} else if (window.innerWidth > 1024) {
+	// 		console.log(readMore, window.innerWidth, containerHeight, 'ddd');
+	// 		if (filteredList.length > 12 && !readMore) {
+	// 			setContainerHeight('992px');
+	// 		} else {
+	// 			setContainerHeight('fit-content');
+	// 		}
+	// 	} else if (window.innerWidth > 768) {
+	// 		if (filteredList.length > 12 && !readMore) {
+	// 			setContainerHeight('771.64px');
+	// 		} else {
+	// 			setContainerHeight('fit-content');
+	// 		}
+	// 	} else {
+	// 		if (filteredList.length > 12 && !readMore) {
+	// 			setContainerHeight('870px');
+	// 		} else {
+	// 			setContainerHeight('fit-content');
+	// 		}
+	// 	}
+	// };
 
 	useEffect(() => {
 		//handleResize();
-		const observer = new ResizeObserver(handleResize);
-		const targetElement = document.querySelector('.insights')!;
-		observer.observe(targetElement);
+		// const observer = new ResizeObserver(handleResize);
+		// observer.observe(targetElement);
+
+		switch (selectedTab) {
+			case 0:
+				setFilteredList(insightList);
+				break;
+			case 1:
+				setFilteredList(
+					insightList.filter((insight) => insight.type == 'White Paper'),
+				);
+				break;
+			case 2:
+				setFilteredList(
+					insightList.filter((insight) => insight.type == 'Publication'),
+				);
+				console.log(selectedTab);
+				break;
+			case 3:
+				setFilteredList(
+					insightList.filter((insight) => insight.type == 'News'),
+				);
+				break;
+			case 4:
+				setFilteredList(
+					insightList.filter((insight) => insight.type == 'Education'),
+				);
+				break;
+		}
+		// const targetElement = document.querySelector('.insights')!;
+		// if (readMore) {
+		// 	setFilteredList(filteredList);
+		// } else {
+		// 	setFilteredList((prev) => {
+		// 		const newFilteredList = [];
+		// 		for (let i = 0; i < Math.min(12, prev.length); i++) {
+		// 			newFilteredList.push(prev[i]);
+		// 		}
+		// 		return newFilteredList;
+		// 	});
+		// 	// setSlicedList(filteredList.slice(0, 12));
+		// }
 		return () => {};
-	}, [filteredList, readMore, containerHeight, window.innerWidth]);
+	}, [readMore, containerHeight, selectedTab]);
 
 	return (
 		<Insights
@@ -118,6 +163,8 @@ const InsightsContainer = (props: Props) => {
 			onReadMoreClick={onReadMoreClick}
 			containerHeight={containerHeight}
 			selectedTab={selectedTab}
+			prevHeight={prevHeight}
+			slicedList={slicedList}
 		/>
 	);
 };

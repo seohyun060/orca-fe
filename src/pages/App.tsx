1ca855aff361645e1e696c0ef71dd9f8dac3ab17
Noah <seohyun060@naver.com>
2023-10-12 11:32:14 +0900

    fix

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,17 +5,24 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import ReactGA from 'react-ga';
 import '../styles/core.css';
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
+
+// 환경 변수가 정의되어 있을 때만 Google Analytics 초기화
+if (gaTrackingId) {
+	ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
+	ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
+}
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 function App() {
-  return (
-    <Provider store={store}>
-      <RootNavigationContainer />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<RootNavigationContainer />
+		</Provider>
+	);
 }
 
 export default App;

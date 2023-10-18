b8560e98cc34135af4507b3628a2c1e50461ffb8
Noah <seohyun060@naver.com>
2023-10-18 11:50:35 +0900

    fix

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -10,10 +10,10 @@ import '../styles/core.css';
 const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
 
 // 환경 변수가 정의되어 있을 때만 Google Analytics 초기화
-if (gaTrackingId) {
-	ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
-	ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
-}
+// if (gaTrackingId) {
+// 	ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
+// 	ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
+// }
 
 const store = createStore(rootReducer, composeWithDevTools());
 

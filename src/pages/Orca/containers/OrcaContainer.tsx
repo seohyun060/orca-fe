5199a7e697942a9875ceeda2d0898eb210915184
Noah <seohyun060@naver.com>
2023-11-01 11:31:14 +0900

    fix

--- src/pages/Orca/containers/OrcaContainer.tsx
+++ src/pages/Orca/containers/OrcaContainer.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Orca from '../Orca';
 import { getOrcaBanner } from 'src/api/BannerAPI';
+import { useLocation } from 'react-router-dom';
 type Props = {};
 
 const OrcaContainer = (props: Props) => {
 	const [orcaBanner, setOrcaBanner] = useState('');
 	const [mobileWidth, setMobileWidth] = useState(768);
+	const location = useLocation();
 	const handleResize = () => {
 		setMobileWidth(window.innerWidth);
 		console.log(mobileWidth);
@@ -17,16 +19,17 @@ const OrcaContainer = (props: Props) => {
 		return () => {
 			window.removeEventListener('resize', handleResize);
 		};
-	}, [window.innerWidth]);
+	}, [window.innerWidth, location]);
 
 	useEffect(() => {
+		setMobileWidth(window.innerWidth);
 		getOrcaBanner().then((data) => {
 			console.log(data.data); // 나옴
 			setOrcaBanner(data.data);
 		});
 		return () => {};
 	}, []);
-
+	console.log(mobileWidth);
 	return <Orca orcaBanner={orcaBanner} mobileWidth={mobileWidth} />;
 };
 
